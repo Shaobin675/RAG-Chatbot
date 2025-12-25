6ce64affd1e18855aa9eb1914923e58ed74e760d
shaobin675 <shaobin4724@gmail.com>
2025-12-25 15:41:17 -0500

    main

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ ORCHESTRATOR_URL = "http://orchestrator:8000"
 RAG_SERVICE_URL = "http://rag-service:8001"
 EMBEDDING_SERVICE_URL = "http://embedding-service:8002"
 LANGGRAPH_SERVICE_URL = "http://langgraph-service:8003"
+# Upload service (HTTP)
+UPLOAD_SERVICE_URL= "http://upload-service:9000"
+# Ingestion worker (NOT for embeddings; admin/health only)
+INGESTION_SERVICE_URL="http://ingestion-worker:9001"
 
 POSTGRES_HOST = "postgres"
 POSTGRES_PORT = 5432
@@ -11,3 +15,7 @@ REDIS_PORT = 6379
 
 # Idle timeout for frontend
 IDLE_TIMEOUT_SECONDS = 180
+
+# Pinecone
+PINECONE_API_KEY=xxx
+PINECONE_INDEX=documents
